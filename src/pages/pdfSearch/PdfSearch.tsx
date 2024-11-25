608964091f1cca9d31e648cdd283f15d430a003d
Mahammadkaif Maniyar <mahammadkaifmaniyar@Mahammadkaifs-MacBook-Air.local>
2024-11-25 23:58:46 +0530

    feature/ISA-33-search-pdf-ui: branch merged

--- src/pages/pdfSearch/PdfSearch.tsx
+++ src/pages/pdfSearch/PdfSearch.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { Input, Button, Pagination, Card, Space, Upload, message } from 'antd';
 import { SearchOutlined, UploadOutlined } from '@ant-design/icons';
-import { Worker, Viewer } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import { pdfjs } from 'react-pdf';
+// import { Worker, Viewer } from '@react-pdf-viewer/core';
+// import '@react-pdf-viewer/core/lib/styles/index.css';
+// import { FieldNamesType } from 'antd/es/cascader';
+// import { pdfjs } from 'react-pdf';
 
 interface PDFItem {
   id: string;
@@ -26,12 +27,12 @@ const SearchPage: React.FC = () => {
   };
 
   const handleSearchTypeToggle = () => {
-    setIsFullTextSearch((prevState) => !prevState);
+    setIsFullTextSearch(prevState => !prevState);
   };
 
   const handlePageChange = (pageNumber: number) => setCurrentPage(pageNumber);
 
-  const filteredPdfItems = pdfItems.filter((item) =>
+  const filteredPdfItems = pdfItems.filter(item =>
     item.pdfName.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -39,7 +40,7 @@ const SearchPage: React.FC = () => {
   const endIndex = startIndex + itemsPerPage;
   const displayedItems = filteredPdfItems.slice(startIndex, endIndex);
 
-  const totalPages = Math.ceil(filteredPdfItems.length / itemsPerPage);
+  // const totalPages = Math.ceil(filteredPdfItems.length / itemsPerPage);
 
   // Function to handle the View PDF
   const handleViewClick = (pdfName: string) => {
@@ -47,7 +48,7 @@ const SearchPage: React.FC = () => {
   };
 
   // Upload handle function
-  const handleUpload = (file: any) => {
+  const handleUpload = (file: File) => {
     // Only allow PDF files
     const isPDF = file.type === 'application/pdf';
     if (!isPDF) {
@@ -55,41 +56,38 @@ const SearchPage: React.FC = () => {
       return false;
     }
     message.success(`${file.name} file uploaded successfully`);
-    return false; 
+    return false;
   };
 
   return (
-    <div className="flex justify-center items-start p-4 min-h-screen bg-gray-50 relative overflow-hidden">
-      <div className="w-full max-w-4xl flex flex-col h-full">
-        
-        <div className="absolute top-4 right-4 z-10">
+    <div className='flex justify-center items-start p-4 min-h-screen bg-gray-50 relative overflow-hidden'>
+      <div className='w-full max-w-4xl flex flex-col h-full'>
+        <div className='absolute top-4 right-4 z-10'>
           <Upload
             beforeUpload={handleUpload} // Handle file before upload
             showUploadList={false} // Disable default upload list
-            accept=".pdf" // Only allow PDF files
+            accept='.pdf' // Only allow PDF files
           >
-            <Button icon={<UploadOutlined />} type="primary">
+            <Button icon={<UploadOutlined />} type='primary'>
               Upload PDF
             </Button>
           </Upload>
         </div>
 
         {/* Header Section */}
-        <div className="flex flex-col md:flex-row justify-between items-center mb-2 mt-6">
-          <div className="relative w-full md:w-3/4 lg:w-4/5 mb-2 md:mb-0">
-            
+        <div className='flex flex-col md:flex-row justify-between items-center mb-2 mt-6'>
+          <div className='relative w-full md:w-3/4 lg:w-4/5 mb-2 md:mb-0'>
             <Search
-              placeholder="Enter the keyword to search" 
+              placeholder='Enter the keyword to search'
               value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)} 
-              onSearch={handleSearch} 
+              onChange={e => setSearchTerm(e.target.value)}
+              onSearch={handleSearch}
               enterButton={<SearchOutlined />}
-              className="w-full border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-lg"
+              className='w-full border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-lg'
             />
           </div>
 
-         
-          <Space size={20} className="mt-2 md:mt-0">
+          <Space size={20} className='mt-2 md:mt-0'>
             <Button
               onClick={handleSearchTypeToggle}
               type={isFullTextSearch ? 'primary' : 'default'}
@@ -106,15 +104,19 @@ const SearchPage: React.FC = () => {
         </div>
 
         {/* Cards Grid Section */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-4 flex-grow overflow-hidden">
-          {displayedItems.map((item) => (
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-4 flex-grow overflow-hidden'>
+          {displayedItems.map(item => (
             <Card
               key={item.id}
               title={item.pdfName}
-              extra={<Button onClick={() => handleViewClick(item.pdfName)}>View PDF</Button>}
+              extra={
+                <Button onClick={() => handleViewClick(item.pdfName)}>
+                  View PDF
+                </Button>
+              }
               style={{ width: '100%' }}
               hoverable
-              className="shadow-lg rounded-lg"
+              className='shadow-lg rounded-lg'
             >
               <p>{item.pageNumbers}</p>
             </Card>
@@ -122,7 +124,7 @@ const SearchPage: React.FC = () => {
         </div>
 
         {/* Pagination Section */}
-        <div className="flex justify-center mt-2">
+        <div className='flex justify-center mt-2'>
           <Pagination
             current={currentPage}
             total={filteredPdfItems.length}
@@ -135,7 +137,6 @@ const SearchPage: React.FC = () => {
     </div>
   );
 };
-
 
 function generateDummyData(numItems: number): PDFItem[] {
   return Array.from({ length: numItems }, (_, i) => ({
